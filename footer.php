b6071cd495995b0b1b950844870cedab7954b996
diannty <101880835+diannty@users.noreply.github.com>
2022-06-19 14:26:04 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>&copy; 2022 Klinik Jasmine, Dian Tri Handayani - 312010041</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+include("koneksi.php");
+
+// query untuk menampilkan data
+
+$sql = 'SELECT * FROM kliinik_312010041';
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title >KLINIK JASMINE</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1 align="center">Sistem Informasi Klinik Jasmine</h1>
+        </header>
+      
